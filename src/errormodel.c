2a39bcb8d95958ba354e85d8837c4474d35a83c1
j-dr <jderose@stanford.edu>
2015-11-12 13:20:20 -0800

    implemented addgals style error model, need u band 24th mag flux and sky flux

--- src/errormodel.c
+++ src/errormodel.c
@@ -50,3 +50,47 @@ void apply_uniform_errormodel(float exptime[], float limmags[], float lnscat, in
       omagerr[i] = 1.086*fluxerr[i]/flux[i];
     }
 }
+
+void add_des_photometric_errors(float maglim[], float sig, int nband,
+				vector<float> &mag, vector<float> &flux,
+				vector<float> &fluxerr, vector<float> &omag,
+				vector<float> &omagerr)
+{
+  const float nk[] = {4.6, 12.9, 17.7, 45.1, 14.9}; //flux for sky at 24th magnitude
+  float es[] = {9.95, 10.01, 8.02, 6.18, 0.812}; //flux for a 24th magnitude galaxy
+  float exposure_time[nband];
+  float zps[nband];
+  seed = time(NULL);
+  std::default_random_engine generator(seed);
+  std::normal_distribution<float> rng();
+  const float apperture = 1.5;   //angular size of a typical galaxy
+  const float pixel = 0.27;      //angular size of a detector pixel
+  const float apperture_area = M_PI * pow( apperture / 2.0, 2 );
+  const float pixels_area = pow( pixel, 2 );
+  const int npixels = apperture_area / pixels_area;
+
+
+  //maglim = [24.0, 24.0, 24.0, 24.0, 24.0, 24.0]
+  
+  for (i=0; i<nband; i++)
+    {
+      zps[i] = 24. + 2.5 * log10( es[i] );  //zero-point for flux calculation of a galaxy
+      es[i] = pow(10, 0.4 * ( zps[i] - maglim[i] ) );  //recalibrate our 10-sigma fluxes to detection limits
+      exposure_time[i] = pow( 10.0 / es[i], 2 ) * ( nk[i] * npixels + es[i] );
+    }
+
+  for (vector<float>::iterator itr=mag.begin(); itr!=mag.end; itr++)
+    {
+      i = itr-mag.begin();
+      b = i%nband;
+      flux[i] = pow( 10.0, -0.4 * (*mag - zps[b]) ) * exposure_time[b];
+      fluxerr[i] = sqrt( nk[b] * exposure_time[b] * npixels + flux[i] );
+      flux[i] += rng(generator) * fluxerr[i];
+      if (flux[i] < 0){
+	omag[i] = 99.0;
+      }	else {
+	omag[i] = zps[b] - 2.5 * alog10( flux[i] / exposure_time[i] );
+      }
+      omagerr[i] = 2.5 * log10( 1.0 + fluxerr[i] / flux[i] );
+    }
+}
